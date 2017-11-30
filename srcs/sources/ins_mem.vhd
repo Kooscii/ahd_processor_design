5e3699f99e6a6f6f864fcaf7ed3991a5f72f058e
Unknown <sandy.huang1994@hotmail.com>
2017-11-30 00:08:55 -0500

    add write function
    According to the project requirement, add write function to the previous design. Meanwhile, update testbench and report.

--- srcs/sources/ins_mem.vhd
+++ srcs/sources/ins_mem.vhd
@@ -2,30 +2,29 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 11/14/2017 07:51:14 PM
+-- Create Date:    08:10:34 11/22/2017 
 -- Design Name: 
--- Module Name: ins_mem - Behavioral
+-- Module Name:    ins_mem - Behavioral 
 -- Project Name: 
 -- Target Devices: 
--- Tool Versions: 
+-- Tool versions: 
 -- Description: 
--- 
+--
 -- Dependencies: 
--- 
--- Revision:
+--
+-- Revision: 
 -- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Additional Comments: 
+--
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.STD_LOGIC_ARITH.ALL;
---use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -34,7 +33,9 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ins_mem is
     Port ( inst : out STD_LOGIC_VECTOR (31 downto 0);
-           addr : in STD_LOGIC_VECTOR (31 downto 0));
+           addr : in STD_LOGIC_VECTOR (31 downto 0);
+              wd: in STD_LOGIC_VECTOR (31 downto 0);
+              w_clk: in STD_LOGIC);
 end ins_mem;
 
 architecture Behavioral of ins_mem is
@@ -298,25 +299,13 @@ architecture Behavioral of ins_mem is
     x"ffffffff"
         );
 begin
---	process(addr) 
---		subtype word is STD_LOGIC_VECTOR (31 downto 0);
---		type memory is array(0 to 7) of word;--can be changed later
---		variable myMem: memory:= (
---		    X"00000001", 
---		    X"00000010",
---			X"00000100", 
---			X"00001000",
---			X"00010000", 
---			X"00100000",
---			X"01000000", 
---			X"10000000"
---			);
---		begin
---			inst<=myMem(conv_integer(addr(31 downto 2)));
---		end process;
 
-    inst<=myMem( TO_INTEGER( UNSIGNED( addr(31 downto 2))));
+     process(w_clk)
+     begin
+        if rising_edge(w_clk) then
+        myMem(TO_INTEGER( UNSIGNED(addr(31 downto 2))))<=wd;
+        end if;
+     end process;
+     inst<=myMem( TO_INTEGER( UNSIGNED( addr(31 downto 2))));
     
 end Behavioral;
-
-
